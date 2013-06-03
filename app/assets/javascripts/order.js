59bbdde43426eb88287b0508d8f4457c02e97825
Kham <piotr.szeremeta@gmail.com>
2013-06-03 23:32:26 +0200

    big picture works with js

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,5 +1,40 @@
 var PAYMILL_PUBLIC_KEY = '22916269435eef4b426e05604ce8cc89';
 
+var slogan_format = function(phrase){
+  var part_title;
+  var intitle = phrase.split(' ').slice(0,5);
+  $.each(intitle, function(index, word) {
+    part_title = $('<div />', {
+      text: word,
+      id: 't' + index,
+      class: 'caption title'});
+    $('.big_product_image').append(part_title);
+  });
+  return 0;
+};
+
+
+function update_big_image () {
+ var title = $(this).data('title');
+ var image_url = $(this).data('photo');
+
+ $('.big_product_image .image').css({
+  'background-image': 'url("' + image_url + '")',
+  'background-position': '20% 0%',
+  'background-repeat': 'no-repeat'
+});
+
+ $('.big_product_image .caption').remove();
+
+ slogan_format(title);
+
+ $(".product_thumbnail, .product .title a").on('mouseleave', function(){
+  $('.big_product_image .caption.title').remove();
+  $('.big_product_image .image').css("background","white");
+  slogan_format("Best prices");
+});
+}
+
 $(function() {// Stuff to do as soon as the DOM is ready;
   $("#payment-form").submit(function(event) {
     event.preventDefault();
@@ -20,7 +55,7 @@ $(function() {// Stuff to do as soon as the DOM is ready;
   });
 
   function PaymillResponseHandler(error, result) {
-       
+
    $('#transaction_card-number').val(' '); // required, ohne Leerzeichen und Bindestriche
     $('#transaction_card-expiry-month').val(' '); // required
     $('#transaction_card-expiry-year').val(' ');   // required, vierstellig z.B. "2016"
@@ -29,12 +64,12 @@ $(function() {// Stuff to do as soon as the DOM is ready;
    $('#transaction_card-currency').val('');// required, ISO 4217 z.B. "EUR" od. "GBP"
    $('#transaction_card-holdername').val(' ');
 
-    if (error) {
+   if (error) {
     // Shows the error above the form
     $(".payment-errors").text(error.apierror);
     $(".submit-button").removeAttr("disabled");
   } else {
- var form = $("#payment-form");
+   var form = $("#payment-form");
     // Output token
     var token = result.token;
     // Insert token into form in order to submit to server
@@ -45,40 +80,8 @@ $(function() {// Stuff to do as soon as the DOM is ready;
 
   }
 }
-var slogan_format = function(phrase){
-  var part_title;
-  var intitle = phrase.split(' ').slice(0,5);
-  $.each(intitle, function(index, word) {
-    part_title = $('<div />', {
-      text: word,
-      id: 't' + index,
-      class: 'caption title'});
-    $('.big_product_image').append(part_title);
-  });
-  return 0;
-};
 
 slogan_format("Best prices");
-
-$(".product_thumbnail, .product .title a").on('mouseenter', function(){
-  var title = $(this).data('title');
-  var image_url = $(this).data('photo');
-
-  $('.big_product_image .image').css({
-    'background-image': 'url("' + image_url + '")',
-    'background-position': '20% 0%',
-    'background-repeat': 'no-repeat'
-  });
-
-  $('.big_product_image .caption').remove();
-
-  slogan_format(title);
-
-  $(".product_thumbnail, .product .title a").on('mouseleave', function(){
-    $('.big_product_image .caption.title').remove();
-    $('.big_product_image .image').css("background","white");
-    slogan_format("Best prices");
-  });
-});
+$(".product_thumbnail, .product .title a").on('mouseenter', update_big_image);
 
 });
