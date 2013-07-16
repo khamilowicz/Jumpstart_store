1df74679c0dc8a1b1eb7b8ad48be669c44dac2ea
Kham <piotr.szeremeta@gmail.com>
2013-07-16 22:14:33 +0200

    added js grid tweaks of product.short and big_picture

--- app/assets/javascripts/all/product.js
+++ app/assets/javascripts/all/product.js
@@ -5,5 +5,30 @@ $(function() {
   $('.cart_btn').bind('ajax:success', function() {
     $(this).button('reset');
   });
+
   $('.carousel').carousel();
+
+  widthChanges();
+  $(window).resize(widthChanges);
 });
+
+function widthChanges (){
+  if (window.innerWidth > 1200) {
+    ProductGrid(3);
+    console.log('hi');
+  };
+  if (window.innerWidth < 1200 && window.innerWidth > 768) {
+    ProductGrid(2);
+  };
+};
+
+var ProductGrid = function(amount_in_row){
+  if (amount_in_row == 2) {
+    $('.product.short').removeClass('span4');
+    $('.product.short').addClass('span6');
+  };
+  if (amount_in_row == 3) {
+    $('.product.short').removeClass('span6');
+    $('.product.short').addClass('span4');
+  };
+};
